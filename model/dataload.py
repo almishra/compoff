1885bf31a20ce38dac0c50de7a9a07af137055b9
smeet <schheda@cs.stonybrook.edu>
2021-10-14 09:44:38 -0400

    setting up dataloader

---
+++ model/dataload.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, Dataloader
+from sklearn.preprocessing import StandardScaler as std_scaler
+
+class CompData(Dataset):
+    def __init__(self, X, y, train=True, scaler=True, meta_train_batch=32, meta_test_batch=32, test_batch=32):
+        
+                
+        self.scaler = scaler
+        if train:
+            self.meta_train_batch = meta_train_batch
+            self.meta_test_batch = meta_test_batch
+        else:
+            self.test_batch = test_batch
+        
+        if scaler:
+            self.X = std_scaler().fit_transform(X)
+        
+        ## Add followin to __getitem__
+        #if not torch.is_tensor(train_x):
+        #    self.train_x = torch.from_numpy(train_x)
+        #if not torch.is_tensor(train_y):
+        #    self.train_y = torch.from_numpy(train_y)
+
+
+
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self,index):
+        ### create model and check input sizes and then create matrices with that dimension [meta_train_batch, shape of 1 data row]
+
+
+
---
+++ model/main_worker.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import arparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import sys
+from dataload import CompData
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_tr', type=int, default=32,\
+        help=" [option] meta train batch size for each task model")
+parser.add_argument('--batch_te', type=int, default=32,\
+        help=" [option] meta test batch size for each task model")
+parser.add_argument('--train_test_split', type=float, default=0.6,\
+        help=" [option] split data into train and test sets")
+parser.add_argument('--task_num', type=int, default=3,\
+        help=" [option] number of task models")
+parser.add_argument('--lr', type=float, dafault=1e-3,\
+        help=" [option] outer learning rate")
+parser.add_argument('--decay', type=float, default=0.0,\
+        help=" [option] L2 penalty for outer optimization algorithm")
+#### Add support for learning rate decay
+#parser.add_argument('--lr_decay', type=float, default=0.95,\
+#        help=" [option] learning rate decay multiplicative factor" )
+
+####  Update this to application to test on (generalization setting)
+parser.add_argument('--app', type=str, default="matrix_multiplication",\
+        help=" [option] choose application to train & test")
+parser.add_argument('--dataset_root', type=str, \
+        help=" [option] path to dataset root directory")
+parser.add_argument('--split_seed', type=int, default=43,\
+        help=" [option] train-test split seed (can be used fpr reproducibility)")
+#parser.add_argument('')
+
+
+def train():
+
+
+
+
+def test():
+
+
+
+
+
+
+def main():
+    args = parser.parse_args()
+    if not args.dataset_root:
+        sys.exit("Enter pat to dataset root directory")
+
+    print('<script option> ',args)
+    worker(args)
+
+
+def worker():
+    
+    dr_columns = ['kernel','Compiler','Cluster','gpu_name','outer','inner','var_decl','ref_expr','int_literal','float_literal','mem_to',\
+                'mem_from','add_sub_int','add_sub_double','mul_int','mul_double','div_int','div_double','assign_int','assign_double']
+    
+    if args.dataset_root[-1] != "/":
+        args.dataset_root += "/"
+
+    if args.app == "matrix_multiplication":
+        try:
+            df = pd.read(args.dataset_root+"matrix_multiplication.csv")   
+            df = df.drop(columns=dr_columns)
+        except:
+            print('An error occured')
+        finally:
+            sys.exit("please check the dataset path and file names")
+    
+    X = df.iloc[:, 0:-1]
+    y = df.iloc[:, -1]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=args.train_test_split, random_state=args.split_seed)
+    
+    train_sets = CompData(X_train,y_train, train=True, meta_train_batch=args.batch_tr, meta_test_batch=args.batch_te)
+    test_sets = CompData(X_test, y_test, train=False, test_batch=32)
+
+
+
+if __name__ == '__main__':
+    main()
+
