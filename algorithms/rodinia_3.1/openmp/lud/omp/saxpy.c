f30ae166031a8a597e78f10196f436307677249f
CaptainSharf <mohdsharf7407@gmail.com>
2022-02-08 15:34:05 -0500

    Measuring time for omp

--- algorithms/rodinia_3.1/openmp/lud/omp/saxpy.c
+++ algorithms/rodinia_3.1/openmp/lud/omp/saxpy.c
@@ -10,7 +10,7 @@
 #define B(_i,_j) b[_i*matrix_dim+_j]
 #define C(_i,_j) c[_i*matrix_dim+_j]
 
-void saxpy(float* a, float* b, float* c, int matrix_dim){
+void saxpy_normal(float* a, float* b, float* c, int matrix_dim){
     int i,j,k;
     stopwatch sw;
     stopwatch_start(&sw);
@@ -26,8 +26,24 @@ void saxpy(float* a, float* b, float* c, int matrix_dim){
     printf("Time consumed(ms): %lf\n", 1000*get_interval_by_sec(&sw));
 }
 
+void saxpy_omp(float* a, float* b, float* c, int matrix_dim){
+    int i,j,k;
+    stopwatch sw;
+    stopwatch_start(&sw);
+    #pragma omp parallel for
+    for(i=0;i<matrix_dim;i++){
+        for(j=0;j<matrix_dim;j++){
+            float totalSum = 0.0f;
+            for(k=0;k<matrix_dim;k++)
+                totalSum+=A(i,k)*B(k,j);
+            C(i,j) = totalSum;
+        }
+    }
+    stopwatch_stop(&sw);
+    printf("Time consumed(ms): %lf\n", 1000*get_interval_by_sec(&sw));
+}
 int main(int argc, char* argv[]){
-    int matrix_dim = 32;
+    int matrix_dim = 1000;
     float *mat1, *mat2, *res;
     int ret1 = create_matrix(&mat1, matrix_dim);
     int ret2 = create_matrix(&mat2, matrix_dim);
@@ -35,6 +51,7 @@ int main(int argc, char* argv[]){
     if(ret1!=RET_SUCCESS || ret2!=RET_SUCCESS || ret3!=RET_SUCCESS){
         exit(0);
     }
-    saxpy(mat1, mat2, res, matrix_dim);
+    saxpy_normal(mat1, mat2, res, matrix_dim);
+    saxpy_omp(mat1, mat2, res, matrix_dim);
     return 0;
 }
