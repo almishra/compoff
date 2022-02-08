c4925b93f61e0b15b15be0d95fb9da5e07a7795e
CaptainSharf <mohdsharf7407@gmail.com>
2022-02-08 14:22:44 -0500

    Fixing float**

--- algorithms/rodinia_3.1/openmp/lud/omp/saxpy.c
+++ algorithms/rodinia_3.1/openmp/lud/omp/saxpy.c
@@ -1,21 +1,27 @@
-#include <omp.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <assert.h>
 
 #include "common.h"
 
-// void saxpy(float* a, float* b){
-// }
+void saxpy(float* a, float* b){
+}
 int main(int argc, char* argv[]){
     int matrix_dim = 32;
-    float *mat;
-    int ret = create_matrix(&mat, matrix_dim);
-    int i;
-    for(i=0;i<matrix_dim;i++){
-        int ii;
-        for(ii=0;ii<matrix_dim;ii++){
-            printf("%.6f ", mat[i*matrix_dim+ii]);
-        }
-        printf("\n");
+    float *mat1, *mat2;
+    int ret1 = create_matrix(&mat1, matrix_dim);
+    int ret2 = create_matrix(&mat2, matrix_dim);
+    if(ret1!=RET_SUCCESS || ret2!=RET_SUCCESS){
+        exit(0);
     }
+    // for(i=0;i<matrix_dim;i++){
+    //     int ii;
+    //     for(ii=0;ii<matrix_dim;ii++){
+    //         printf("%.6f ", mat[i*matrix_dim+ii]);
+    //     }
+    //     printf("\n");
+    // }
     return 0;
 }
