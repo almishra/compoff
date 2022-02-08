0e86c9747f3460d1ca1ce64f957312bea98567fa
CaptainSharf <mohdsharf7407@gmail.com>
2022-02-08 13:27:46 -0500

    Adding test saxpy

---
+++ algorithms/rodinia_3.1/openmp/lud/omp/saxpy.c
@@ -0,0 +1,22 @@
+#include <omp.h>
+#include <stdio.h>
+
+#include "common.h"
+
+// void saxpy(float* a, float* b){
+
+// }
+int main(int argc, char* argv[]){
+    int matrix_dim = 32;
+    float *mat;
+    int ret = create_matrix(mat, matrix_dim);
+    int i;
+    for(i=0;i<matrix_dim;i++){
+        int ii;
+        for(ii=0;ii<matrix_dim;ii++){
+            printf("%d ", mat[i*matrix_dim+ii]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
