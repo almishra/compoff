8038d89f422f2fe44a8b58d9fa3120e3e862d786
CaptainSharf <mohdsharf7407@gmail.com>
2022-02-09 16:08:22 -0500

    Adding vector version of saxpy

---
+++ algorithms/rodinia_3.1/openmp/lud/omp/saxpy_vector.c
@@ -0,0 +1,19 @@
+#include<omp.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+void create_vector(float* vector, int dim) {
+    vector = (float*) malloc(sizeof(float)*dim);
+    int i;
+    #pragma parallel for shared(vector) private(i)
+    for(i=0;i<dim;i++){
+        vector[i] = ((float) rand()/(float)(RAND_MAX));
+    }
+}
+
+int main(int argc, char* argv[]){
+    float* curr_vector;
+    create_vector(curr_vector, 1000);
+    return 0;
+}
