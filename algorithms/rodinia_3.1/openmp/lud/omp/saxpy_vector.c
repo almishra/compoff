acd7d1b23efe482150da438baf2e6bd96b3ad282
CaptainSharf <mohdsharf7407@gmail.com>
2022-02-09 16:28:37 -0500

    verifying vector

--- algorithms/rodinia_3.1/openmp/lud/omp/saxpy_vector.c
+++ algorithms/rodinia_3.1/openmp/lud/omp/saxpy_vector.c
@@ -3,21 +3,21 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-void create_vector(float* vector, int dim) {
-    vector = (float*) malloc(sizeof(float)*dim);
+void create_vector(float** vector, int dim) {
+    float* curr = (float*) malloc(sizeof(float)*dim);
     int i;
-    #pragma omp parallel for shared(vector) private(i)
+    #pragma omp parallel for shared(curr) private(i)
     for(i=0;i<dim;i++){
-        vector[i] = ((float) rand()/(float)(RAND_MAX));
+        curr[i] = ((float) rand()/(float)(RAND_MAX));
     }
+    *vector = curr;
 }
 
 int main(int argc, char* argv[]){
     float* curr_vector;
     int i, vector_dim = 10;
     create_vector(curr_vector, vector_dim);
-    // #pragma omp parallel for
-    for(i=0;i<10;i++)
+    for(i=0;i<vector_dim;i++)
         printf("%.6f ", curr_vector[i]);
     printf("\n");
     return 0;
