68776f21fab9e1a1bae7ef66a5657c180557d9ac
Sharfuddin Mohammed <shmmohammed@fj-epyc.cm.cluster>
2022-03-03 17:44:46 -0500

    Adding run script for lud

---
+++ algorithms/rodinia_3.1/openmp/lud/omp/run.sh
@@ -0,0 +1,4 @@
+clang -c -fopenmp -fopenmp-targets=nvptx64 -Xopenmp-target -march=sm_70 -I../common lud.c -o lud.o -DOMP_OFFLOAD
+clang -c -fopenmp -fopenmp-targets=nvptx64 -Xopenmp-target -march=sm_70 -I../common lud_omp.c -o lud_omp.o -DOMP_OFFLOAD
+clang -c -fopenmp -fopenmp-targets=nvptx64 -Xopenmp-target -march=sm_70 -I../common ../common/common.c -o ../common/common.o -DOMP_OFFLOAD
+clang -fopenmp -fopenmp-targets=nvptx64 -L/lustre/projects/compoff-group/opt/llvm/lib -Xopenmp-target -march=sm_70 -o lud_omp lud.o lud_omp.o ../common/common.o -lm  -DOMP_OFFLOAD
