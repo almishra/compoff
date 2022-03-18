03773a983642016ceb5ff04b1cb9f6681839deef
Alok Mishra <alokmishra.besu@gmail.com>
2022-03-17 22:31:57 -0400

    Updating run files for v100 and k80 for wilson variants

--- algorithms/wilson_dslash/variants/run_k80.sh
+++ algorithms/wilson_dslash/variants/run_k80.sh
@@ -2,6 +2,12 @@ for ((i=4; i<=64; i+=4))
 do
   echo $i;
   t=$(( 2 * i ));
-  ./dslash_${i}_${i}_${i}_${t}_k80.out;
-  ./dslash_${i}_${i}_${i}_${t}_k80_memcpy.out;
+  if [ ! -f output_dslash_${i}_${i}_${i}_${t}_k80.csv ]
+  then
+    ./dslash_${i}_${i}_${i}_${t}_k80.out;
+  fi
+  if [ ! -f output_dslash_${i}_${i}_${i}_${t}_k80.csv ]
+  then
+    ./dslash_${i}_${i}_${i}_${t}_k80_memcpy.out;
+  fi
 done
--- algorithms/wilson_dslash/variants/run_v100.sh
+++ algorithms/wilson_dslash/variants/run_v100.sh
@@ -2,6 +2,12 @@ for ((i=4; i<=64; i+=4))
 do
   echo $i;
   t=$(( 2 * i ));
-  ./dslash_${i}_${i}_${i}_${t}_v100.out;
-  ./dslash_${i}_${i}_${i}_${t}_v100_memcpy.out;
+  if [ ! -f output_dslash_${i}_${i}_${i}_${t}_v100.csv ]
+  then
+    ./dslash_${i}_${i}_${i}_${t}_v100.out;
+  fi
+  if [ ! -f output_dslash_${i}_${i}_${i}_${t}_v100_memcpy.csv ]
+  then
+    ./dslash_${i}_${i}_${i}_${t}_v100_memcpy.out;
+  fi
 done
