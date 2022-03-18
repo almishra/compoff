6e49ebd1c1c00afc35bab798b9f7905a1637e208
CaptainSharf <mohdsharf7407@gmail.com>
2022-03-18 04:10:29 -0400

    Adding newline

--- algorithms/rodinia_3.1/openmp/lud/omp/lud_omp.c
+++ algorithms/rodinia_3.1/openmp/lud/omp/lud_omp.c
@@ -169,6 +169,6 @@ lud_diagonal_omp(a, size, offset);
 stopwatch_stop(&sw);
 int usecs = get_interval_by_usec(&sw);
 double secnds = get_interval_by_sec(&sw);
-printf("%lu,%lu,%d,%f",(size*(size+1))*sizeof(int), size*size*sizeof(int), \
+printf("%lu,%lu,%d,%f\n",(size*(size+1))*sizeof(int), size*size*sizeof(int), \
                 usecs, secnds);
 }
