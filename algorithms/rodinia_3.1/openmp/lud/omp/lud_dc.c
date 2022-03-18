e484aec67cafc228e1e4649cfce7c332add96f87
CaptainSharf <mohdsharf7407@gmail.com>
2022-03-18 03:27:36 -0400

    Adding LUD data collection file

---
+++ algorithms/rodinia_3.1/openmp/lud/omp/lud_dc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "common.h"
+
+extern void
+lud_omp(float *m, int matrix_dim);
+
+int main ( int argc, char *argv[]){
+    int matrix_dim = 100;
+    for(;matrix_dim<=1000;){
+        int ret = create_matrix(&m, matrix_dim);
+        if (ret != RET_SUCCESS) {
+            m = NULL;
+            fprintf(stderr, "error create matrix internally size=%d\n", matrix_dim);
+        }
+        lud_omp(m, matrix_dim);
+        matrix_dim*=10;
+    }
+}
