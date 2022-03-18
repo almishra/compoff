2dc562019bbf7c43c57641f144d33f9d7666d4d2
CaptainSharf <mohdsharf7407@gmail.com>
2022-03-18 03:36:01 -0400

    Adding LUD data collection file

--- algorithms/rodinia_3.1/openmp/lud/omp/lud_dc.c
+++ algorithms/rodinia_3.1/openmp/lud/omp/lud_dc.c
@@ -11,6 +11,7 @@ lud_omp(float *m, int matrix_dim);
 
 int main ( int argc, char *argv[]){
     int matrix_dim = 100;
+    float *m;
     for(;matrix_dim<=1000;){
         int ret = create_matrix(&m, matrix_dim);
         if (ret != RET_SUCCESS) {
