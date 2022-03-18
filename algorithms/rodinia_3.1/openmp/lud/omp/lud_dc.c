526dce000793a182e6a87289fdf58d5cb3961b88
CaptainSharf <mohdsharf7407@gmail.com>
2022-03-18 05:32:21 -0400

    Adding lud bm data

--- algorithms/rodinia_3.1/openmp/lud/omp/lud_dc.c
+++ algorithms/rodinia_3.1/openmp/lud/omp/lud_dc.c
@@ -14,20 +14,20 @@ int main ( int argc, char *argv[]){
     printf("matrix_dim,mem_to,mem_from,runtime(us), runtime(s)");
     for(;matrix_dim<=1000;){
         float *m;
-        stopwatch sw;
+        //stopwatch sw;
         int ret = create_matrix(&m, matrix_dim);
         if (ret != RET_SUCCESS) {
             m = NULL;
             fprintf(stderr, "error create matrix internally size=%d\n", matrix_dim);
         }
-        stopwatch_start(&sw);
-        lud_omp(m, matrix_dim);
-        stopwatch_stop(&sw);
-        int usecs = get_interval_by_usec(&sw);
-        double secnds = get_interval_by_sec(&sw);
-        int size = matrix_dim;
-        printf("%d,%lu,%lu,%d,%f\n",size, (size*(size+1))*sizeof(int), size*size*sizeof(int), \
-                usecs, secnds);
+        // stopwatch_start(&sw);
+        // lud_omp(m, matrix_dim);
+        // stopwatch_stop(&sw);
+        // int usecs = get_interval_by_usec(&sw);
+        // double secnds = get_interval_by_sec(&sw);
+        // int size = matrix_dim;
+        // printf("%d,%lu,%lu,%d,%f\n",size, (size*(size+1))*sizeof(int), size*size*sizeof(int), \
+        //         usecs, secnds);
         matrix_dim*=10;
     }
 }
