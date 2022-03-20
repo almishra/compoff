3e26f2a7defc823bfbd3fd5b396d0aba49fda4cb
CaptainSharf <mohdsharf7407@gmail.com>
2022-03-20 17:44:32 -0400

    Adding loop

--- algorithms/rodinia_3.1/openmp/lud/omp/lud.c
+++ algorithms/rodinia_3.1/openmp/lud/omp/lud.c
@@ -46,74 +46,6 @@ main ( int argc, char *argv[] )
   func_ret_t ret;
   const char *input_file = NULL;
   float *m, *mm;
-
-	
-  // while ((opt = getopt_long(argc, argv, "::vs:n:i:", 
-  //                           long_options, &option_index)) != -1 ) {
-  //   switch(opt){
-  //   case 'i':
-  //     input_file = optarg;
-  //     break;
-  //   case 'v':
-  //     do_verify = 1;
-  //     break;
-  //   case 'n':
-  //     omp_num_threads = atoi(optarg);
-  //     break;
-  //   case 's':
-  //     matrix_dim = atoi(optarg);
-  //     printf("Generate input matrix internally, size =%d\n", matrix_dim);
-  //     // fprintf(stderr, "Currently not supported, use -i instead\n");
-  //     // fprintf(stderr, "Usage: %s [-v] [-s matrix_size|-i input_file]\n", argv[0]);
-  //     // exit(EXIT_FAILURE);
-  //     break;
-  //   case '?':
-  //     fprintf(stderr, "invalid option\n");
-  //     break;
-  //   case ':':
-  //     fprintf(stderr, "missing argument\n");
-  //     break;
-  //   default:
-  //     fprintf(stderr, "Usage: %s [-v] [-s matrix_size|-i input_file]\n",
-	//       argv[0]);
-  //     exit(EXIT_FAILURE);
-  //   }
-  // }
-  
-  // if ( (optind < argc) || (optind == 1)) {
-  //   fprintf(stderr, "Usage: %s [-v] [-n no. of threads] [-s matrix_size|-i input_file]\n", argv[0]);
-  //   exit(EXIT_FAILURE);
-  // }
-
-  // if (input_file) {
-  //   printf("Reading matrix from file %s\n", input_file);
-  //   ret = create_matrix_from_file(&m, input_file, &matrix_dim);
-  //   if (ret != RET_SUCCESS) {
-  //     m = NULL;
-  //     fprintf(stderr, "error create matrix from file %s\n", input_file);
-  //     exit(EXIT_FAILURE);
-  //   }
-  // }
-  // else if (matrix_dim) {
-  //   printf("Creating matrix internally size=%d\n", matrix_dim);
-  //   ret = create_matrix(&m, matrix_dim);
-  //   if (ret != RET_SUCCESS) {
-  //     m = NULL;
-  //     fprintf(stderr, "error create matrix internally size=%d\n", matrix_dim);
-  //     exit(EXIT_FAILURE);
-  //   }
-  // }
- 
-  // else {
-  //   printf("No input file specified!\n");
-  //   exit(EXIT_FAILURE);
-  // } 
-
-  // if (do_verify){
-  //   printf("Before LUD\n");
-  //   /* print_matrix(m, matrix_dim); */
-  //   matrix_duplicate(m, &mm, matrix_dim);
-  // }
   for(;matrix_dim<=1000;){
     ret = create_matrix(&m, matrix_dim);
     if (ret != RET_SUCCESS) {
@@ -129,14 +61,6 @@ main ( int argc, char *argv[] )
     free(m);
     matrix_dim*=10;
   }
-
-  // if (do_verify){
-  //   printf("After LUD\n");
-  //   /* print_matrix(m, matrix_dim); */
-  //   printf(">>>Verify<<<<\n");
-  //   lud_verify(mm, m, matrix_dim); 
-  //   free(mm);
-  // }
 
   return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
