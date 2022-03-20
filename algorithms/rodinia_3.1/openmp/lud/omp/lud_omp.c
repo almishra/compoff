f3811f23ed99d5203a10430aa6d7d698d4d033c5
CaptainSharf <mohdsharf7407@gmail.com>
2022-03-20 18:02:49 -0400

    Adding loop

--- algorithms/rodinia_3.1/openmp/lud/omp/lud_omp.c
+++ algorithms/rodinia_3.1/openmp/lud/omp/lud_omp.c
@@ -45,6 +45,7 @@ void lud_diagonal_omp (float* a, int size, int offset)
 // implements block LU factorization 
 void lud_omp(float *a, int size)
 {
+    printf("called\n");
     int offset, chunk_idx, size_inter, chunks_in_inter_row, chunks_per_inter;
     stopwatch sw;
     stopwatch_start(&sw);
